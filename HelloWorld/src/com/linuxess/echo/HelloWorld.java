cbdb88f0680ba9f7af7297b020b4144f903659a0
unknown <qingjian.wlt@hz.ali.com>
2016-06-12 16:55:34 +0800

    Initial commit

---
+++ HelloWorld/src/com/linuxess/echo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.linuxess.echo;
+
+/**
+ * Created by qingjian.wlt on 2016/6/8.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
